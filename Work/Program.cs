eaf696a89f8ef0a37daec2b6ffd7e66adcda0510
buddy <budman@list.ru>
2022-04-04 13:10:02 +0500

    Подготовка и вывод на печать

--- Work/Program.cs
+++ Work/Program.cs
@@ -22,3 +22,15 @@ void SecondArrayWithIF(string[] array1, string[] array2)
         }
     }
 }
+
+//Подготовка и вывод на печать.
+void PrintArray(string[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.Write($"{array[i]} ");
+    }
+    Console.WriteLine();
+}
+SecondArrayWithIF(array1, array2);
+PrintArray(array2);
